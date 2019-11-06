bea05b3f8a72623079db0f325946832579475cae
Dmitry Nogin <v-dmnogi@microsoft.com>
2019-11-06 13:48:27 -0800

    Implement Board validation

---
+++ Sudoku/Board.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Linq.Enumerable;
+
+namespace Sudoku
+{
+    public class Board
+    {
+        public Board(int? [,] cells)
+        {
+            Cells = cells ?? throw new ArgumentNullException(nameof(cells));
+        }
+
+        int? [,] Cells { get; }
+        int N => Cells.Length;
+
+        public bool Valid => ColumnsValid && RowsValid && GridsValid;
+
+        bool RowsValid => Rows.All(AllUnique);
+        bool ColumnsValid => Columns.All(AllUnique);
+        bool GridsValid => Grids.All(AllUnique);
+
+        IEnumerable<IEnumerable<int>> Rows => 
+            Range(0, N).Select(r => Numbers(0, N - 1, r, r));
+        IEnumerable<IEnumerable<int>> Columns => 
+            Range(0, N).Select(c => Numbers(c, c, 0, N - 1));
+        IEnumerable<IEnumerable<int>> Grids => 
+            from r in Range(0, N / 3)
+            from c in Range(0, N / 3)
+            select Numbers(c * 3, c * 3 + 2, r * 3, r * 3 + 2);
+               
+        bool AllUnique(IEnumerable<int> numbers) =>
+            numbers.Distinct().Count() == numbers.Count();
+
+        IEnumerable<int> Numbers(int left, int right, int top, int bottom) =>
+            from r in Range(top, bottom - top + 1)
+            from c in Range(left, right - left + 1)
+            where Cells[r, c].HasValue
+            select Cells[r, c].Value;
+    }
+}
